995dedd3beb16a6362120d9ddaa00d196fd4b4f2
hamzehmhe <hamzeh.mh61@gmaile.com>
2023-12-24 07:53:00 +0330

    5

--- src/json file2.py
+++ src/json file2.py
@@ -17,7 +17,7 @@ for i in data:
       
   #    size = (i[size])
    #   color = (i[color])
-    #  print(f'{size} is {color}')
+   # # #  print(f'{size} is {color}')
     #dfg709i7hfhlklsdf55pop978754
 
 print("----------------end---------------------")
